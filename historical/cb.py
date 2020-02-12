7a6aa64e3f3e512d32b5482b5dcfa666324674c1
Jamis Johnson <jamismanwaring@gmail.com>
2020-02-12 18:14:04 -0500

    move away from multi-threaded. retries data retrieval when rate-limiting encountered

--- historical/cb.py
+++ historical/cb.py
@@ -11,42 +11,131 @@ import keys
 
 # external libs
 import numpy as np
+import pandas as pd
 import cbpro
+import pystore
+pystore.set_path('./pystore')
 
 # cb client
-cb = cbpro.AuthenticatedClient(keys.apiKey, keys.apiSecret, keys.passphrase)
+# cb = cbpro.AuthenticatedClient(keys.apiKey, keys.apiSecret, keys.passphrase)
+cb = cbpro.PublicClient()
 
 ###
 ### iteratively acquire entire ETH-USD Coinbase market history
 ###
 market = 'ETH-USD'
+
+# ready function to retrieve data multi-threaded
+get_hist = partial(cb.get_product_historic_rates, market)
+
 # start date of ETH-USD on Coinbase
-start = dt(year=2017, month=5, day=18)
+# start = dt(year=2017, month=5, day=18) NOTE: the real start date
+# start = dt(year=2017, month=6, day=18)
 today = dt.today()
 # start = dt(year=2016, month=5, day=18)
-with PoolExecutor(max_workers=6) as executor:
-    while True:
-        ### do it multi-threaded (bursts of 6 requests per second allowed)
-        get_hist = partial(cb.get_product_historic_rates, market)
 
-        # list of tuple function args for multi-processing
-        # each tuples is a time window
-        time_windows =  [(start + timedelta(minutes=300*i), # start of window
-                          start + timedelta(minutes=300*(i+1)), # end of window
-                          60, # interval in seconds
-                         ) for i in range(0,6)]
+frames = 3 # number of frames fetched asynchronously
+width = 300 # number of results per time frame
+granularity = 60 # avail: 60 (min), 300 (5min), 900 (15min), 3600 (1hr), 21600 (6hr), 86400 (1day)
 
-        res = executor.map(get_hist, *zip(*time_windows))
+def _gen_date_frames(start, until=None, width=300):
+    """Generator for list of sequential time-window tuples.
 
-        import ipdb; ipdb.set_trace();
-        # TODO store candle data in database
-        # TODO time.sleep for some calculated amount
-        # TODO output any request errors along with start datetime information
+    Args:
+        start (datetime): Start datetime of list of dates
+        until (datetime): Generate list until this date is hit
+        width (int): Width of each time frame in minutes
+    """
+    end = start + timedelta(minutes=width)
+    while start <= until if until else dt.today():
+        yield start, end
+        start = end + timedelta(minutes=width)
+        end = start + timedelta(minutes=width)
 
-        for r in res:
-            print(len(r))
-            import ipdb; ipdb.set_trace();
 
-        # run up until today's date
-        if any([d['start'] > today for d in time_windows]):
+
+def _get_data(market, start, end, granularity, attempts=10, sleep=2):
+    """Retrieve historical data from Coinbase API.
+    Attempts resiliency to rate limiting by delaying 
+    new requests after halted.
+
+    Args:
+        start (datetime): Start datetime of historical data window.
+        end (datetime): End datetime of historical data window.
+        granularity (int): Candle granularity in seconds. 
+            options: 60 (min), 300 (5min), 900 (15min), 3600 (1hr), 
+            21600 (6hr), 86400 (1day)
+        attempts (int): Number of attempts made after rate limiting.
+        sleep (int): Sleep in seconds after rate limiting. 
+            Increases by one multiple each time.
+    Returns:
+        list: List of candle data from Coinbse corresponding to `start` and `end`.
+    """
+    def _is_rate_limited(res):
+        return isinstance(res, dict) and 'rate limit' in res['message'].lower()
+
+    def _sleep_on_rate_limit(res, multiplier=1):
+        seconds = sleep*multiplier
+        print('\nerror :{0:s}\n--- sleeping {1:} seconds ---\n'.format(res['message'], seconds))
+        time.sleep(seconds)
+
+    def _retrieve_data():
+        return cb.get_product_historic_rates(market, start, end, granularity)
+
+    res = _retrieve_data()
+    # if rate limited, sleep and try to retrieve data again and again
+    for count, _ in enumerate(range(0, attempts)):
+        if not _is_rate_limited(res):
             break
+        _sleep_on_rate_limit(res, count+1)
+        res = _retrieve_data()
+    res.reverse()
+    return res
+
+
+
+if __name__ == "__main__":
+    exec_time = time.time()
+
+    start_date = dt(year=2017, month=6, day=18)
+    dates = _gen_date_frames(start_date)
+
+    # TODO make pystore vars as program args
+    store = pystore.store('store_')
+    collection = store.collection('coll_')
+
+    for count, (start, end) in enumerate(dates):
+        # CB API limited to 3 reqs/sec
+        time.sleep(0.1)
+
+        # retrieve data from CB API
+        res = _get_data(market, start, end, granularity)
+
+        # python -> pandas time-series dataframe
+        df = pd.DataFrame(res, columns=['unixtime', 'low', 'high', 'open', 'close', 'volume'])
+        df['unixtime'] = pd.to_datetime(df['unixtime'], unit='s')
+        df = df.set_index('unixtime')
+
+        # TODO confirm this data structure is the one we want to be storing.
+        #      mostly, indexing by pd.DateTime object
+
+        # write dataframe to data store
+        if count == 0: # first run, create data store
+            # TODO what metadata do we want?
+            collection.write('ethusd_', df, metadata={'granularity':granularity})
+        else: # all other runs, append data
+            collection.append('ethusd_', df, npartitions=1)
+
+
+        # LOGGING
+        # TODO setup logging file
+        print('count', count)
+        print('start', start)
+        print('end  ', end)
+        print('res len:', len(res))
+
+        elapsed_time = time.time() - exec_time
+        avg_exec_time =  elapsed_time / (count+1.0)
+        print("avg secs/req {0:.4f}".format(avg_exec_time))
+        print()
+
